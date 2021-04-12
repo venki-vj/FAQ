f4813328aec6694df26f3c2cddd404c05c67d1a1
venki-vj <73542029+venki-vj@users.noreply.github.com>
2021-04-12 20:09:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -80,27 +80,7 @@ app.use(flash());
 app.use(helmet());
 
 
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://*.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
-];
+
 const fontSrcUrls = [];
 app.use(
     helmet.contentSecurityPolicy({
